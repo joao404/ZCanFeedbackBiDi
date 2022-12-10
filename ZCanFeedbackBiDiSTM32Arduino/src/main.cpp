9212037fe07c1db3c3ce900595f9d874e69066c8
Marcel Maage <mmaage92@googlemail.com>
2022-12-10 12:28:04 +0100

    Deactivate Debugging

--- ZCanFeedbackBiDiSTM32Arduino/src/main.cpp
+++ ZCanFeedbackBiDiSTM32Arduino/src/main.cpp
@@ -49,7 +49,7 @@ int configIdPin1{PB13};
 
 // I will need in the end two of those moduls to handle each of the 8 inputs
 FeedbackDecoder feedbackDecoder1(memoryData.modulConfig1, Flash::writeData, trackPin1, detectionMode1,
-                                 configRailcomPin, configIdPin1, xprintf, true, true, true);
+                                 configRailcomPin, configIdPin1, xprintf, true, false, false);
 
 std::array<int, 8> trackPin2{PB9, PB8, PB7, PB6, PB5, PB4, PB3, PA15};
 
@@ -58,7 +58,7 @@ int configIdPin2{PB14};
 
 // I will need in the end two of those moduls to handle each of the 8 inputs
 FeedbackDecoder feedbackDecoder2(memoryData.modulConfig2, Flash::writeData, trackPin2, detectionMode2,
-                                 configRailcomPin, configIdPin2, xprintf, true, true, false);
+                                 configRailcomPin, configIdPin2, xprintf, true, false, false);
 
 int ledPin{PC13};
 uint32_t lastLedBlinkINms{0};
